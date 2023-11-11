a5fe2ecf108084e255e63fb0324015985975a2f5
barkure <43804451+barkure@users.noreply.github.com>
2023-11-11 16:09:42 +0800

    生成Docker镜像

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# 使用官方 Python 运行时作为父镜像
+FROM python:3.9-slim
+
+# 设置工作目录为 /app
+WORKDIR /app
+
+# 将当前目录内容复制到容器的 /app 中
+ADD . /app
+
+# 安装在 requirements.txt 中列出的任何需要的包
+RUN pip install --no-cache-dir -r requirements.txt
+
+# 使端口 1234 可供此应用使用
+EXPOSE 1234
+
+# 运行 main.py 时，容器将运行以下命令
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- config.py
+++ config.py
@@ -1,17 +1,30 @@
-# 定义 GitHub OAuth 配置
-GITHUB_CLIENT_ID = "your-github-client-id"
-GITHUB_CLIENT_SECRET = "your-github-client-secret"
-# GitHub回调地址
-REDIRECT_URI = "http://localhost:3000/github-oauth-redirect" # 此项根据前端地址修改
+# # 定义 GitHub OAuth 配置
+# GITHUB_CLIENT_ID = "your-github-client-id"
+# GITHUB_CLIENT_SECRET = "your-github-client-secret"
+# # 前端地址
+# FRONTEND_URL = "http://localhost:8080"
+# # 配置 JWT 密钥
+# SECRET_KEY = "your-secret-key"
+# # 配置TOKEN过期时间
+# ACCESS_TOKEN_EXPIRE_MINUTES = 1440 # 默认24小时
+
+# 如果你要自己前后端分离部署，那么你需要取消上面的注释，按需修改配置项，且应该注释掉下面的配置项。
+# 从环境变量中获取配置项
+import os
+GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+# 前端地址
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+# 配置 JWT 密钥
+SECRET_KEY = os.getenv("SECRET_KEY")
+# 配置TOKEN过期时间
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))  # 默认24小时
+
+
+# 以下是公共配置，无需修改
 # GitHub授权链接，此项不用修改
 GITHUB_OAUTH_URL = f"https://github.com/login/oauth/authorize?client_id={GITHUB_CLIENT_ID}&redirect_uri={REDIRECT_URI}"
-
-# 前端地址，此项根据前端地址修改
-FRONTEND_URL = "http://localhost:3000" # 此项根据前端地址修改
-# 配置 JWT 密钥和算法
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256" # 默认是 HS256 算法
-
-# 配置TOKEN过期时间
-ACCESS_TOKEN_EXPIRE_MINUTES = 1440 # 默认24小时
-
+# GitHub回调地址
+REDIRECT_URI = f"{FRONTEND_URL}/github-oauth-redirect"
+# JWT 算法
+ALGORITHM = "HS256" 
